93178a5c95e67f2ae09058c325c96770dc47e34a
Pinhook <contact@pinhooklabs.com>
2011-04-11 16:44:54 -0500

    first part ready

--- scripts/contacts/contacts.js
+++ scripts/contacts/contacts.js
@@ -1,13 +1,15 @@
-require([
+require(['acme/style/style',
          'acme/list/list'], function(){
 	
 
 	
-	$("#artists").acme_list('/services/categories');
-	
-	$("#albums").acme_list('/services/companies');
-	
-	$("#genres").acme_list('/services/locations');
+	$("#categories ul").acme_list('/services/categories');
 	
 	
+	$("#locations ul").acme_list('/services/locations');
+	
+	$("#companies ul").acme_list('/services/companies');
+	
+	
+	$("#contacts ul").acme_list('/services/contacts');
 })
