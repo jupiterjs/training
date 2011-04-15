1ced58d5e86b31bdab74956e0d383f4246c4723c
Pinhook <contact@pinhooklabs.com>
2011-04-14 23:48:42 -0500

    removed dynamic fixtures

--- 0/contacts/fixtures/fixtures.js
+++ 0/contacts/fixtures/fixtures.js
@@ -1,5 +1,7 @@
-require(['jupiter/fixture/fixture','contacts/fixtures/contacts'], function(){
+require(['jupiter/fixture/fixture'], function(){
 	$.jupiter_fixture('/services/categories','fixtures/categories.json');
 	$.jupiter_fixture('/services/locations','fixtures/locations.json');
 	$.jupiter_fixture('/services/companies','fixtures/companies.json');
-})
+});
+
+// TODO : 'contacts/fixtures/contacts'
