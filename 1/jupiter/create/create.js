a066025a23d56b8740307e02ff55b3d151eda468
Pinhook <contact@pinhooklabs.com>
2011-04-15 01:12:14 -0500

    create works

--- 1/jupiter/create/create.js
+++ 1/jupiter/create/create.js
@@ -1,8 +1,31 @@
 require(['jquery'], function($){
 	
 $.fn.jupiter_create = function(options){
-	this.element.click(function(){
-		$(options.form).prependTo(options.prependTo)
+	
+	this.click(function(){
+		
+		$(options.form)
+			.prependTo(options.prependTo)
+			.submit(function(ev){
+				var form = $(this),
+					data = {};
+				
+				form.find('input').each(function(i, input){
+					data[input.name] = input.value;
+				})
+				
+				ev.preventDefault();
+				
+				$.post(options.url, 
+					data,
+					function(attrs){
+						form.remove();
+					}, 
+					'json');
+				
+				$(this).text("Loading ...");
+			})
+			
 	})
 };
 
